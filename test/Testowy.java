39e465a2e7d93c8ae3950fe7df8ce5cede645515
Dawid Krzykawski <162042870+ChesterDanes@users.noreply.github.com>
2025-05-10 12:52:24 +0200

    Update Testowy.java

--- test/Testowy.java
+++ test/Testowy.java
@@ -4,6 +4,6 @@ import org.junit.Test;
 public class Testowy {
     @Test
     public void testAdd() {
-        assertEquals(5, Hello.add(2, 3));
+        assertEquals(5, HeyTest.add(2, 3));
     }
 }
