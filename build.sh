8805d8d5e65b8cb0efbd7d665c7146b1d72621e8
Dawid Krzykawski <162042870+ChesterDanes@users.noreply.github.com>
2025-05-10 12:42:18 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ mkdir -p out
 mkdir -p test-reports
 
 # Kompilacja
-javac -d out -cp lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar src/Hello.java test/HelloTest.java
+javac -d out -cp lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar src/HeyTest.java test/Testowy.java
 
 # Uruchomienie testów z zapisem do pliku
 java -cp out:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar \
-    org.junit.runner.JUnitCore HelloTest \
+    org.junit.runner.JUnitCore Testowy \
     | tee test-reports/test-output.txt
 
 # Konwersja na XML
