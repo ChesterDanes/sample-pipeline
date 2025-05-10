f729a806f7ab0dfe43a99bd8901d1428958895dd
Dawid Krzykawski <162042870+ChesterDanes@users.noreply.github.com>
2025-05-10 12:48:27 +0200

    Create Testowy.java

---
+++ test/Testowy.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Testowy {
+    @Test
+    public void testAdd() {
+        assertEquals(5, Hello.add(2, 3));
+    }
+}
